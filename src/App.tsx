c15a1ab9d8fcf1ce9ecbf31e945bd2c8196be866
paraparata <srizalmp@gmail.com>
2021-12-06 22:16:10 +0700

    first fly

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.root}>
+      <div>Navbar</div>
+      <main>Main</main>
+    </div>
+  );
+}
+
+export default App;
